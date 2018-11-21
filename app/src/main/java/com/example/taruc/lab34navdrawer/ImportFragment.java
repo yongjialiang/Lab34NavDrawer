0d080a12b8375c28b9cbca9de6428049da58724d
yongjialiang <yongjl-wa15@student.tarc.edu.my>
2018-11-21 18:06:28 +0800

    Initial

---
+++ app/src/main/java/com/example/taruc/lab34navdrawer/ImportFragment.java
@@ -0,0 +1,46 @@
+package com.example.taruc.lab34navdrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImportFragment extends Fragment {
+
+
+    public ImportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_import, container, false);
+
+        final TextView textViewMessage;
+        textViewMessage = view.findViewById(R.id.textViewMessage);
+        Button buttonShowMessage;
+        buttonShowMessage = view.findViewById(R.id.buttonShowMessage);
+        buttonShowMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewMessage.setText("HELLO FRAGMENT WEJOQJEWH");
+            }
+        });
+        // Inflate the layout for this fragment
+
+        return view;
+    }
+
+
+
+}
